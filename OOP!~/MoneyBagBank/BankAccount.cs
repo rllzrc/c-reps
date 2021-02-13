12450b39b238cc1de1ecb220b9b19fc3152d1ea5
Rella <rellas.email@gmail.com>
2021-02-12 20:54:06 -0500

    build: Add base files for MBB program with BankAccount class + constructor

--- OOP!~/MoneyBagBank/BankAccount.cs
+++ OOP!~/MoneyBagBank/BankAccount.cs
@@ -4,10 +4,20 @@ using System.Text;
 
 namespace MoneyBagBank {
   public class BankAccount {
+    // properties 
+    // these are the "form fields"
     public string Number { get; }
     public string Owner { get; set; }
     public decimal Balance { get; }
 
+    // constructor to make the bank account
+    // think: this is the form 
+    public BankAccount(string name, decimal initialBalance) {
+      this.Owner = name;
+      this.Balance = initialBalance;
+    }
+
+    // methods 
     public void MakeDeposit(decimal amount, DateTime date, string note) {
 
     }
---
+++ OOP!~/MoneyBagBank/MoneyBagBank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoneyBagBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Henlo~ World!");
+            var account = new BankAccount("Izzi", 20000000);
+            Console.WriteLine($"Ayyy!~ Welcome to MoneyBagBankGG! Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+        }
+    }
+}
