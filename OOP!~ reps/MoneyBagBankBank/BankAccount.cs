d90eafabc918b36a44995d932fde82fd31066c7c
rllzrc <rellas.email@gmail.com>
2021-02-14 00:17:52 -0500

    build: Update Balance property in BankAccount class to enumerate all transactions + provides a sum as its current balance -- fix error on reassignment issue in line 28 for later

--- OOP!~ reps/MoneyBagBankBank/BankAccount.cs
+++ OOP!~ reps/MoneyBagBankBank/BankAccount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 // to provide a logical way to structure the codebase -> this app is v small so all the code will reside in this one namespace
 namespace MoneyBagBankBank {
@@ -7,9 +8,19 @@ namespace MoneyBagBankBank {
     // these are the "form fields" aka properties -> data/elements (can have additional code to enforce type validation + other rules)
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; } 
+    // to sum up all the values within all transactions => this computation enumerates all transactions and provides the sum as the current balance
+    public decimal Balance { get {
+      decimal balance = 0;
+      foreach(var item in allTransactions) {
+        balance += item.Amount;
+      }
+      return balance;
+      }
+    }
     // not the best way to create an account number per say, but since this app is tiny this will suffice to generate a unique number for each account created => can only be accessed by code inside the BankAccount class -> implementation to generate numbers must remain private -> static = shared by all of the BankAccount objects (the value of a non-static variable is unique to each instance of the BankAccount object)
     private static int accountNumberSeed = 1234567890;
+    // pulling data from Transactions class into a list of objects to extract out later
+    private List<Transaction> allTransactions = new List<Transaction>();
 
     // to open a new account -> create a new object from the BankAccount class to define, assign values, + initialize objects of this same class type. AKA the C O N S T R U C T O R ~ => can be called when you create a new object using the new keyword
     public BankAccount(string name, decimal initialBalance) {
