8c69c418e9f0a4e47bd6f2f195ed5b3aa92b5e87
rllzrc <rellas.email@gmail.com>
2021-02-18 16:37:52 -0500

    build: Add comments examples from single, multiple, + XML types to exercise Program class

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,5 +1,22 @@
 ﻿using System;
 
+/*
+this is an example of a multi-line comment! YAY!
+namespace = named groups that contain classes -> all classes must be in a namespace
+classes = aka "Program" here => these are named groups that contain code, as all code must be in a class
+main method = aka the entry point of the program, where it starts when it is ran
+*/
+
+// * .NET Framework => programming infrastructure => FCL = framework class library = a collection of resuable classes, interfaces, and types
+
+// XML documentation comments ->
+/// <summary>
+/// The below class performs an important function, the main entry point to our exercise Program
+/// This will build an XML file of comments upon compilation with the /doc option
+// It is to show info to the user through Intellisense
+/// This is not metadata; it is not included in the compiled assembly + therefore not accessible through reflection.
+/// </summary>
+
 namespace Fundamentals
 {
     class Program
